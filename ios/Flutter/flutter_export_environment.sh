8a65db084fcf893eadcba4bd84f7fe9d74dcdd7c
John Willemstad Osuyos <124412006+wllmstd@users.noreply.github.com>
2023-10-04 16:06:50 +0800

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter_windows_3.13.0-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Kaye\Desktop\BSIT\BSIT 3B\APP DEV & EMERGING TECH\Exercise 4\cc206_cashlens"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter\flutter_application_cashlens"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
