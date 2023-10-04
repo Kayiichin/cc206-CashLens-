d132273c48a7e3f76486ac292c3aeba26bafd6dd
John Willemstad Osuyos <124412006+wllmstd@users.noreply.github.com>
2023-10-04 18:25:33 +0800

    Update settings_page.dart

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SettingsPage());
+  runApp(Feature3());
 }
 
-class SettingsPage extends StatelessWidget {
+class Feature3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
