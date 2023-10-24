dfac284735c4628b80c1ac7fd900520f99cd2dfa
Kaye Bacos Bearneza <124229077+Kayiichin@users.noreply.github.com>
2023-10-24 19:22:01 +0800

    Update dashboard_page.dart

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -7,36 +7,57 @@ class Feature1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CashLens Dashboard'),
+        title: Text('CashLens Dashboard'),
+        backgroundColor: Colors.blue[900],
       ),
-      body: ListView(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          Container(
+            color: Colors.yellow[200],
+            padding: EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                'Welcome to CashLens',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[900],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+
           // Section 1: Banknote Detection
           DashboardBox(
             title: 'Banknote Detection',
-            icon: Icons.account_balance, // You can change the icon
-            notificationCount: 0, // Update with the actual notification count
+            icon: Icons.account_balance,
+            notificationCount: 0,
           ),
+          SizedBox(height: 10),
 
           // Section 2: Currency Conversion
           DashboardBox(
             title: 'Currency Conversion',
-            icon: Icons.monetization_on, // You can change the icon
-            notificationCount: 2, // Update with the actual notification count
+            icon: Icons.monetization_on,
+            notificationCount: 2,
           ),
+          SizedBox(height: 10),
 
           // Section 3: E-Wallet
           DashboardBox(
             title: 'E-Wallet',
-            icon: Icons.account_balance_wallet, // You can change the icon
-            notificationCount: 1, // Update with the actual notification count
+            icon: Icons.account_balance_wallet,
+            notificationCount: 1,
           ),
+          SizedBox(height: 10),
 
           // Section 4: Settings
           DashboardBox(
             title: 'Settings',
-            icon: Icons.settings, // You can change the icon
-            notificationCount: 0, // Update with the actual notification count
+            icon: Icons.settings,
+            notificationCount: 0,
           ),
         ],
       ),
@@ -62,20 +83,25 @@ class DashboardBox extends StatelessWidget {
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'CashLens Dashboard',
+            title,
             style: TextStyle(
               fontSize: 18,
               fontWeight: FontWeight.bold,
             ),
           ),
-          Divider(), // Add a divider for separation
+          Divider(),
           ListTile(
-            leading: Icon(icon),
+            leading: Icon(
+              icon,
+              color: Colors.blue[900], // Set the icon color to dark blue
+            ),
             title: Text(title),
             trailing: notificationCount > 0
                 ? CircleAvatar(
