c9c00877bf12807a6d41aec2cb68ddaa5ffd7fa5
angelicalouisesallones <124368397+angelicalouisesallones@users.noreply.github.com>
2023-10-24 23:44:33 +0800

    Update conversion_page.dart

--- lib/conversion_page.dart
+++ lib/conversion_page.dart
@@ -7,7 +7,13 @@ class ConversionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Currency Conversion'),
+        title: Row(
+          children: [
+            SizedBox(width: 10), // Adjust the spacing
+            Text('CashLens Currency Conversion'),
+          ],
+        ),
+        backgroundColor: Colors.blue[900],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -16,7 +22,7 @@ class ConversionPage extends StatelessWidget {
             // Currency Conversion Box 1: From Currency (Peso)
             ConversionBox(
               title: 'From Currency (Peso)',
-              icon: Icons.attach_money,
+              icon: Icons.money_rounded,
             ),
 
             SizedBox(height: 20), // Add spacing between the boxes
@@ -24,18 +30,26 @@ class ConversionPage extends StatelessWidget {
             // Currency Conversion Box 2: To Currency (Dollar)
             ConversionBox(
               title: 'To Currency (Dollar)',
-              icon: Icons.attach_money,
+              icon: Icons.money_rounded,
             ),
 
             SizedBox(height: 20),
 
-            // Rate information text
-            Text(
-              'Rates as of 19 Sep 2023 9:47 PM\n1 PHP = 0.018 USD',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+            // Rate information text in a yellow box
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.yellow[200],
+              ),
+              child: Text(
+                'Rates as of 19 Sep 2023 9:47 PM\n1 PHP = 0.018 USD',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ],
@@ -57,9 +71,11 @@ class ConversionBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
         children: [
@@ -71,9 +87,15 @@ class ConversionBox extends StatelessWidget {
             ),
           ),
           Divider(),
-          Icon(icon, size: 48, color: Colors.blue),
+          Icon(icon, size: 48, color: Colors.blue[900]),
         ],
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ConversionPage(),
+  ));
 }
