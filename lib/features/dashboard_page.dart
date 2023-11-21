42440c6eb6672708dff2f828ec3e53435531e748
Kaye Bacos Bearneza <124229077+Kayiichin@users.noreply.github.com>
2023-11-21 10:57:35 +0800

    Add files via upload

---
+++ lib/features/dashboard_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:cc206_cashlens/components/home_drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Feature1 extends StatelessWidget {
+  const Feature1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeDrawer()),
+        );
+        return false; // Prevent default back button behavior
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('CashLens Dashboard'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.yellow[200],
+              padding: EdgeInsets.all(16.0),
+              child: Center(
+                child: Text(
+                  'Welcome to CashLens',
+                  style: GoogleFonts.josefinSans(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[900],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            // Section 1: Banknote Detection
+            DashboardBox(
+              title: 'Banknote Detection',
+              icon: Icons.account_balance,
+              notificationCount: 0,
+            ),
+            SizedBox(height: 10),
+
+            // Section 2: Currency Conversion
+            DashboardBox(
+              title: 'Currency Conversion',
+              icon: Icons.monetization_on,
+              notificationCount: 2,
+            ),
+            SizedBox(height: 10),
+
+            // Section 3: E-Wallet
+            DashboardBox(
+              title: 'E-Wallet',
+              icon: Icons.account_balance_wallet,
+              notificationCount: 1,
+            ),
+            SizedBox(height: 10),
+
+            // Section 4: Settings
+            DashboardBox(
+              title: 'Settings',
+              icon: Icons.settings,
+              notificationCount: 0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardBox extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final int notificationCount;
+
+  DashboardBox({
+    required this.title,
+    required this.icon,
+    required this.notificationCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: GoogleFonts.josefinSans(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              icon,
+              color: Colors.blue[900], // Set the icon color to dark blue
+            ),
+            title: Text(title),
+            trailing: notificationCount > 0
+                ? CircleAvatar(
+                    backgroundColor: Colors.red,
+                    radius: 10,
+                    child: Text(
+                      '$notificationCount',
+                      style: GoogleFonts.josefinSans(color: Colors.white),
+                    ),
+                  )
+                : null,
+            onTap: () {
+              // Handle section tap here
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
